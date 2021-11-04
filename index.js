45f1911eecf38342aedccf16a437a6030dda6d03
Phoebe Wiant <pwiant@gmail.com>
2021-11-04 15:51:21 -0400

    committing code until the modify the guess range

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/*------------BOILER PLATE--------------------*/
 const readline = require('readline');
 const rl = readline.createInterface(process.stdin, process.stdout);
 
@@ -6,6 +7,15 @@ function ask(questionText) {
     rl.question(questionText, resolve);
   });
 }
+/*------------------GLOBAL VARIABLES----*/
+let min = 0; 
+let max = 100; 
+
+function randomInt(min, max) {
+  let range = max - min +1; 
+  return min +
+  Math.floor(Math.random() *range);
+}
 
 start();
 
@@ -13,6 +23,27 @@ async function start() {
   console.log("Let's play a game where you (human) make up a number and I (computer) try to guess it.")
   let secretNumber = await ask("What is your secret number?\nI won't peek, I promise...\n");
   console.log('You entered: ' + secretNumber);
-  // Now try and complete the program.
-  process.exit();
+
+  let roboGuess = randomInt(min, max);
+  let sassyRobo = await ask ('I think ur number is '+ roboGuess + "? (y or n)")
+  if (sassyRobo === "y") {
+      console.log("OH YEAH. ROBOTS WIN AGAIN");
+      process.exit()
+  } else {
+    while (sassyRobo !== "y"){
+      let hiLow = await ask("Is it higher or lower? ")
+      console.log(hiLow)
+      if (hiLow === "h"){
+        min = roboGuess;
+        roboGuess = randomInt(min, max);
+        sassyRobo = await ask ('I think ur number is ' + roboGuess + "? (y or n)");
+        
+
+      } else { (hiLow !== "h") 
+        max = roboGuess;
+        roboGuess = randomInt(min, max);
+        sassyRobo = await ask('I think ur number is ' + roboGuess + "? (y or n)");
+      }
+    }
+  }
 }
